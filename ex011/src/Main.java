c560c334558add4096bbae0aec6484474b2addb6
RuanAlmeida <Ruan.aalmeidasantos@gmail.com>
2026-04-16 18:09:32 -0300

    first commit

---
+++ ex011/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) {
+        List<String> lista01 = new ArrayList<>();
+
+        lista01.add("Ruan");
+        lista01.add("Pedro");
+
+        System.out.println(lista01.get(1));
+    }
+}
